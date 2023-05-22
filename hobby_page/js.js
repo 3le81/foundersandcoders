3ce27429093294da76de213b531d79688b250518
Elle81 <lelal@hotmail.com>
2023-05-22 13:47:35 +0100

    project one:hobby page created for foundersandcoders

---
+++ hobby_page/js.js
@@ -0,0 +1,26 @@
+// TIME AND DATE SET 
+
+var clockElement = document.getElementById('clock');
+
+function clock() {
+    var date = new Date();
+
+    // Replace '400px' below with where you want the format to change.
+    if (window.matchMedia('(max-width: 400px)').matches) {
+        // Use this format for windows with a width up to the value above.
+        clockElement.textContent = date.toLocaleString();
+    } else {
+        // While this format will be used for larger windows.
+        clockElement.textContent = date.toString();
+    }
+}
+
+setInterval(clock, 1000);
+
+// TIME DATE END
+
+// ALERT POPUP
+
+function submit() {
+    alert("Thank you for submitting!");
+}
